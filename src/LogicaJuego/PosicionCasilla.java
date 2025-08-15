e631bd7da7074e0e1d269f9fde631d0748e2c2ee
ING Franklin Castillo Umaña <frcastilloum@est.utn.ac.cr>
2025-08-14 21:03:19 -0600

    creacion de la clase PosicionCasilla y sus variables

--- src/LogicaJuego/PosicionCasilla.java
+++ src/LogicaJuego/PosicionCasilla.java
@@ -9,5 +9,52 @@ package LogicaJuego;
  * @author Anguar Alberto Rodriguez Fonseca
  */
 public class PosicionCasilla {
+    private int ubicacionFila; // LA POSICION DE LA CASILLA EN LA FILA
+    private boolean Mina; // SI LA CASILLA TIENE UNA MINA 
+    private int ubicacionColumna; // LA POSICION DE LA CASILLA EN LA COLUMNA
+    private int minasLados; 
+
+    public int getMinasLados() {
+        return minasLados;
+    }
+
+    public void setMinasLados(int minasLados) {
+        this.minasLados = minasLados;
+    }
+     
+//CONSTRUCTOR DE UBICAIONfILA Y UBICACIONCOLUMNA, CON ESTO SE OBTINE LA COORDENADADA DE LA CASILLA
+    public PosicionCasilla(int ubicacionFila, int ubicacionColumna) {
+        this.ubicacionFila = ubicacionFila;
+        this.ubicacionColumna = ubicacionColumna;
+    }
     
+     
+//GETTERS Y SETTERS
+    public int getUbicacionFila() {
+        return ubicacionFila;
+    }
+
+    public void setUbicacionFila(int ubicacionFila) {
+        this.ubicacionFila = ubicacionFila;
+    }
+
+    public boolean isMina() {
+        return Mina;
+    }
+
+    public void setMina(boolean Mina) {
+        this.Mina = Mina;
+    }
+
+    public int getUbicacionColumna() {
+        return ubicacionColumna;
+    }
+
+    public void setUbicacionColumna(int ubicacionColumna) {
+        this.ubicacionColumna = ubicacionColumna;
+    }
+            
+      public void aumentarMinasAlrededor(){ // metodo para recorrer todas las casiilas incrementable
+          this.minasLados++;
+      }
 }
